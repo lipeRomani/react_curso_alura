cbfed2dc02b1fecbcb3a0b4e975dddcdd0f3346b
Felipe Augusto Romani <felipearomani@MacBook-Pro-de-Felipe.local>
2017-05-21 21:11:15 -0300

    implementado HOC

--- src/App.js
+++ src/App.js
@@ -1,59 +1,9 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
+import AutorBox from "./Autor";
 
-class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {autors : [], nome:"", email:"", senha:""};
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-  }
-
-  componentDidMount() {
-    $.ajax({
-      url: "http://localhost:8080/api/autores",
-      method: "get",
-      success: (resp) => {
-        this.setState({autors:resp});
-      }
-    });
-  }
-
-  enviaForm(event) {
-    event.preventDefault();
-    
-    $.ajax({
-      url:"http://localhost:8080/api/autores",
-      method: "POST",
-      contentType: "application/json",
-      data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha:this.state.senha}),
-      success: (resp) => {
-        console.log("Cadastro com sucesso");
-        this.setState({nome:"", email:"", senha:"" });
-        this.setState({autors:resp});
-      },
-      error: (error) => {
-        console.log('cadastro com erro');
-      }
-    })
-  }
-
-  setNome(event){
-    this.setState({nome:event.target.value});
-  }
-
-  setEmail(event){
-    this.setState({email:event.target.value});
-  }
-
-  setSenha(event){
-    this.setState({senha:event.target.value});
-  }
+export default class App extends Component {
 
   render() {
     return (
@@ -79,54 +29,10 @@ class App extends Component {
               <h1>Cadastro de Autores</h1>
             </div>
             <div className="content" id="content">
-              <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label> 
-                    <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}  />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label> 
-                    <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label> 
-                    <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />                                      
-                  </div>
-                  <div className="pure-control-group">                                  
-                    <label></label> 
-                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                  </div>
-                </form>             
-
-              </div>  
-              <div>            
-                <table className="pure-table">
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {
-                      this.state.autors.map((autor) => {
-                        return(
-                            <tr key={autor.id}>
-                              <td>{autor.nome}</td>
-                              <td>{autor.email}</td>
-                            </tr>
-                        );
-                      })
-                    }
-                  </tbody>
-                </table> 
-              </div>             
+              <AutorBox />
             </div>
           </div>            
     </div> 
     );
   }
 }
-
-export default App;
---
+++ src/Autor.js
@@ -0,0 +1,123 @@
+import React, {Component} from "react";
+import InputCustomizado from './componentes/InputCustomizado';
+import SubmitCustomizado from './componentes/SubmitCustomizado';
+import $ from 'jquery';
+
+class FormAutor extends Component {
+    
+    constructor() {
+    super();
+        this.state = {nome:"", email:"", senha:""};
+        this.enviaForm = this.enviaForm.bind(this);
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
+    }
+    
+    render() {
+        return (
+            <div className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                    <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} label="Nome" onChange={this.setNome} />
+                    <InputCustomizado id="email" type="email" name="email" value={this.state.email} label="E-mail" onChange={this.setEmail} />
+                    <InputCustomizado id="senha" type="password" value={this.state.senha} name="senha" label="Senha" onChange={this.setSenha} />
+                    <SubmitCustomizado label="Gravar" />
+                </form>
+            </div>
+        );
+    }
+
+    enviaForm(event) {
+        event.preventDefault();
+    
+        $.ajax({
+            url:"http://localhost:8080/api/autores",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha:this.state.senha}),
+            success: (resp) => {
+                console.log("Cadastro com sucesso");
+                this.setState({nome:"", email:"", senha:"" });
+                this.props.callbackTable(resp);
+            },
+            error: (error) => {
+                console.log('cadastro com erro');
+            }
+        })
+    }
+
+    setNome(event){
+        this.setState({nome:event.target.value});
+    }
+
+    setEmail(event){
+        this.setState({email:event.target.value});
+    }
+
+    setSenha(event){
+        this.setState({senha:event.target.value});
+    }
+}
+
+class TableAutor extends Component {
+
+    render() {
+        return (
+            <div>            
+                <table className="pure-table">
+                  <thead>
+                    <tr>
+                      <th>Nome</th>
+                      <th>email</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      this.props.lista.map((autor) => {
+                        return(
+                            <tr key={autor.id}>
+                              <td>{autor.nome}</td>
+                              <td>{autor.email}</td>
+                            </tr>
+                        );
+                      })
+                    }
+                  </tbody>
+                </table> 
+              </div>
+        );
+    }
+}
+
+export default class AutorBox extends Component {
+    
+    constructor() {
+        super();
+        this.state = {lista : []};
+        this.updateAutorTable = this.updateAutorTable.bind(this); 
+    }
+
+    componentDidMount() {
+        $.ajax({
+            url: "http://localhost:8080/api/autores",
+            method: "get",
+            success: (resp) => {
+                this.setState({lista:resp});
+            }
+        });
+    }
+
+    updateAutorTable(novaLista) {
+        this.setState({lista : novaLista})
+    }
+
+    render() {
+        return (
+            <div>
+                <FormAutor callbackTable={this.updateAutorTable} />
+                <TableAutor lista={this.state.lista}/>
+            </div>
+        );
+    }
+
+}
